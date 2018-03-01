7b27049068ded3ba51792ae1b4d174fb4c6d63ad
skywalker0507 <skywalker0507@gmail.com>
2018-03-01 11:50:11 +0800

    整理项目

--- app/src/main/java/com/skywalker/syntaxhighlighter/MainActivity.java
+++ app/src/main/java/com/skywalker/syntaxhighlighter/MainActivity.java
@@ -2,37 +2,124 @@ package com.skywalker.syntaxhighlighter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
 
+import com.skywalker.syntaxhighlighter.languages.CMode;
 import com.skywalker.syntaxhighlighter.languages.JavaMode;
+import com.skywalker.syntaxhighlighter.languages.JavaScriptMode;
+import com.skywalker.syntaxhighlighter.languages.common.Mode;
+import com.skywalker.syntaxhighlighter.themes.AtomDarkTheme;
 import com.skywalker.syntaxhighlighter.themes.DefaultTheme;
+import com.skywalker.syntaxhighlighter.themes.Theme;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     private HighlightView mHighlightView;
+    private String[] mLanguages = {"c.c", "java.java", "javascript.js"};
+    private Theme[] mThemes = new Theme[2];
+    private Theme mTheme;
+
+    private String[] mCodes = new String[3];
+    private String mCode;
+
+    private Mode[] mModes = {new CMode(), new JavaMode(), new JavaScriptMode()};
+    private Mode mMode;
+
+    private CheckBox mLinenumber;
+    private CheckBox mZoom;
+    private CheckBox mWrapping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mThemes[0] = new DefaultTheme(this);
+        mThemes[1] = new AtomDarkTheme(this);
 
-        try {
+        for (int i = 0; i < mLanguages.length; i++) {
+            try {
+                InputStream inputStream = getAssets().open(mLanguages[i]);
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                inputStream.close();
+                mCodes[i] = new String(buffer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Spinner languages = (Spinner) findViewById(R.id.languagesSpinner);
+        Spinner themes = (Spinner) findViewById(R.id.themesSpinner);
+
+        languages.setSelection(0);
+        themes.setSelection(0);
+        mLinenumber = (CheckBox) findViewById(R.id.checkbox_linenumber);
+        mZoom = (CheckBox) findViewById(R.id.checkbox_zoom);
+        mWrapping = (CheckBox) findViewById(R.id.checkbox_wrapping);
+
+        mLinenumber.setChecked(true);
+        mZoom.setChecked(true);
+        mWrapping.setChecked(false);
+
+        mHighlightView = findViewById(R.id.HighlightView);
+        mHighlightView.setEditable(false);
+        mHighlightView.setShowLineNumber(true);
+        mHighlightView.setWrapping(false);
+
+        languages.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mMode = mModes[position];
+                mCode = mCodes[position];
+
+                try {
+                    mHighlightView.setContent(mCode);
+                    mHighlightView.render(mTheme, mMode);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        themes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mTheme = mThemes[position];
+                mHighlightView.render(mTheme, mMode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        mLinenumber.setOnCheckedChangeListener(this);
+        mWrapping.setOnCheckedChangeListener(this);
+        mZoom.setOnCheckedChangeListener(this);
+        /*try {
             //String file="SyntaxHighlighterParser.java";
-            String file = "ChartComputator.java";
-            //String file="highlight.js";
+            String file = "java.java";
+            //String file="javascript.js";
             InputStream inputStream = getAssets().open(file);
             byte[] buffer = new byte[inputStream.available()];
             inputStream.read(buffer);
             inputStream.close();
 
             //
-            mHighlightView = findViewById(R.id.HighlightView);
+
             mHighlightView.setContent(new String(buffer));
             mHighlightView.setMovementMethod(ScrollingMovementMethod.getInstance());
             HighlightView.Builder builder = new HighlightView.Builder()
@@ -44,26 +131,38 @@ public class MainActivity extends AppCompatActivity {
             mHighlightView.setHighlightBuilder(builder);
             mHighlightView.render(new JavaMode());
 
-            final CheckBox linenumber=findViewById(R.id.checkbox_linenumber);
-            final CheckBox zoom=findViewById(R.id.checkbox_zoom);
-            final CheckBox wrapping=findViewById(R.id.checkbox_wrapping);
-            Button button=findViewById(R.id.button_render);
+
+            Button button = findViewById(R.id.button_render);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    HighlightView.Builder builder = new HighlightView.Builder()
-                            .enableEdit(false)
-                            .setTheme(new DefaultTheme(MainActivity.this))
-                            .showLineNumber(linenumber.isChecked())
-                            .enableZoom(zoom.isChecked())
-                            .textWrapping(wrapping.isChecked());
-                    mHighlightView.setHighlightBuilder(builder);
+
                 }
             });
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
+        }*/
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.checkbox_linenumber:
+                mHighlightView.setShowLineNumber(isChecked);
+                break;
+            case R.id.checkbox_wrapping:
+                mHighlightView.setWrapping(isChecked);
+                break;
+            case R.id.checkbox_zoom:
+                mHighlightView.setZoom(isChecked);
+                break;
+                default:
+                    break;
+
         }
+
+        mHighlightView.render(mMode);
     }
 }
